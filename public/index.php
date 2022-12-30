c9c30f53b0b1d18aabd8fcd4074120f6196a023b
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-12-31 08:25:41 +0900

    Remove hal

--- public/index.php
+++ public/index.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 use BEAR\Hello\Bootstrap;
 
 require dirname(__DIR__) . '/autoload.php';
-exit((new Bootstrap())(PHP_SAPI === 'cli-server' ? 'hal-app' : 'prod-hal-app', $GLOBALS, $_SERVER));
+exit((new Bootstrap())(PHP_SAPI === 'cli-server' ? 'app' : 'prod-app', $GLOBALS, $_SERVER));
